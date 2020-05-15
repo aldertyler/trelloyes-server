c0fb657a13834dabb75dd1995ec86941f0c3bba2
Tyler Alder <tyler.b.alder@gmail.com>
2020-05-14 17:49:03 -0700

    added card and list endpoints

--- src/app.js
+++ src/app.js
@@ -4,17 +4,68 @@ const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
 const { NODE_ENV } = require("./config");
+const winston = require("winston");
 
 const app = express();
 
 const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 
+// set up winston
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.json(),
+  transports: [new winston.transports.File({ filename: "info.log" })],
+});
+
+if (NODE_ENV !== "production") {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.simple(),
+    })
+  );
+}
+
+const cards = [
+  {
+    id: 1,
+    title: "Task One",
+    content: "This is card one",
+  },
+];
+const lists = [
+  {
+    id: 1,
+    header: "List One",
+    cardIds: [1],
+  },
+];
+
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
 
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get("Authorization");
+
+  if (!authToken || authToken.split(" ")[1] !== apiToken) {
+    logger.error(`Unauthorized request to path: ${req.path}`);
+    return res.status(401).json({ error: "Unauthorized request" });
+  }
+  // move to the next middleware
+  next();
+});
+
 app.get("/", (req, res) => {
   res.send("Hello, boilerplate!");
+});
+
+app.get("/card", (req, res) => {
+  res.json(cards);
+});
+
+app.get("/list", (req, res) => {
+  res.json(lists);
 });
 
 app.use(function errorHandler(error, req, res, next) {
